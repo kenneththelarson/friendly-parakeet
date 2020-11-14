c7eb0feff2e0506d439e482ed2c45cde29682619
Kenny <kenneththelarson@gmail.com>
2020-11-14 16:04:57 -0700

    framework

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,31 @@
 // Assignment code here
+function generate() {
 
+  // set password length/complexity
+  let complexity = document.getElementById("slider").value;
+
+  // possible password values
+  let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()-_=+";
+
+  // create for loop to choose password characters
+  for (var i = 0; i <= complexity; i++) {
+    password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.lenght - 1)));
+  }
+
+  // add password to textbox
+  document.getElementById("password").value = password;
+
+}
+
+// set slider default to 60
+document.getElementById("length").innerHTML = "Length: 60";
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generate();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -15,3 +34,22 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// function to set length to slider position
+document.getElementById("slider").oninput = function () {
+
+  if (document.getElementById("slider").value > 8) {
+    document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;
+  }
+  else {
+    document.getElementById("length").innerHTML = "Length: 8";
+  }
+}
+
+// function to copy password to clipboard
+function copyPassword() {
+
+  document.getElementById("display").select();
+  document.execCommand("Copy");
+  alert("Password copied to clipboard");
+}
